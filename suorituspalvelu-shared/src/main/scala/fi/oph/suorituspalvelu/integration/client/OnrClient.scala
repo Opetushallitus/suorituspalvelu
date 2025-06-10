8856616173c597264879c49a7a33c2ea5c8a0ec2
Mikko Siukola <mikko.siukola@cybercom.com>
2025-06-10 16:15:53 +0300

    OPHSUPA-29 Add fetch for masterHenkilos

--- suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/integration/client/OnrClient.scala
+++ suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/integration/client/OnrClient.scala
@@ -3,6 +3,7 @@ package fi.oph.suorituspalvelu.integration.client
 import com.fasterxml.jackson.core.`type`.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import fi.oph.suorituspalvelu.integration.OnrMasterHenkilo
 import fi.vm.sade.javautils.nio.cas.{CasClient, CasClientBuilder, CasConfig}
 import org.asynchttpclient.RequestBuilder
 import org.slf4j.LoggerFactory
@@ -23,6 +24,7 @@ case class Henkiloviite(henkiloOid: String, masterOid: String) {
 
 trait OnrClient {
   def getHenkiloviitteetForHenkilot(personOids: Set[String]): Future[Set[Henkiloviite]]
+  def getMasterHenkilosForPersonOids(personOids: Set[String]): Future[Map[String, OnrMasterHenkilo]]
 }
 
 class OnrClientImpl(casClient: CasClient, environmentBaseUrl: String) extends OnrClient {
@@ -31,23 +33,52 @@ class OnrClientImpl(casClient: CasClient, environmentBaseUrl: String) extends On
 
   val mapper: ObjectMapper = new ObjectMapper()
   mapper.registerModule(DefaultScalaModule)
+  mapper.configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+  val onrBatchSize = 5000
 
 
+  override def getMasterHenkilosForPersonOids(henkiloOids: Set[String]): Future[Map[String, OnrMasterHenkilo]] = {
+    val batches: Seq[(Set[String], Int)] = henkiloOids
+      .grouped(onrBatchSize)
+      .zipWithIndex
+      .toList
+    LOG.info(
+      s"fetch Henkilot in ${batches.size} batches for ${henkiloOids.size} henkilos"
+    )
+    batches.foldLeft(Future(Map[String, OnrMasterHenkilo]())) {
+      case (result: Future[Map[String, OnrMasterHenkilo]], chunk: (Set[String], Int)) =>
+        result.flatMap(rs => {
+          LOG.info(
+            s"Querying onr for Henkilo batch: ${chunk._1.size} oids, batch ${chunk._2 + 1 + "/" + batches.size}"
+          )
+          val chunkResult: Future[Map[String, OnrMasterHenkilo]] = {
+            doPost(environmentBaseUrl + "/oppijanumerorekisteri-service/henkilo/masterHenkilosByOidList", chunk._1)
+              .map(result => {
+                val typeRef = new TypeReference[Map[String, OnrMasterHenkilo]] {}
+                mapper.readValue(result, typeRef)
+              })
+          }
+          chunkResult.map(cr => rs ++ cr)
+        })
+    }
+  }
 
-
-  val batchSize = 5000
   override def getHenkiloviitteetForHenkilot(henkiloOids: Set[String]): Future[Set[Henkiloviite]] = {
-    val batches: Seq[(Set[String], Int)] = henkiloOids.grouped(batchSize).zipWithIndex.toList
+    val batches: Seq[(Set[String], Int)] = henkiloOids.grouped(onrBatchSize).zipWithIndex.toList
 
     val allResults: Seq[Future[Set[Henkiloviite]]] = batches.map((batch: (Set[String], Int)) => {
       LOG.info(s"Haetaan tiedot oppijanumerorekisteristä ${batch._1.size} henkilölle, erä ${batch._2 + 1}/${batches.size}")
 
       val queryObject: Map[String, Set[String]] = Map("henkiloOids" -> batch._1)
-      val batchResult: Future[Set[Henkiloviite]] = doPost(environmentBaseUrl + "/oppijanumerorekisteri-service/s2s/duplicateHenkilos", queryObject)
-        .map(result => {
-          val typeRef = new TypeReference[List[Henkiloviite]] {}
-          mapper.readValue(result, typeRef).toSet
-        })
+      val batchResult: Future[Set[Henkiloviite]] =
+        doPost(environmentBaseUrl + "/oppijanumerorekisteri-service/s2s/duplicateHenkilos", queryObject)
+          .map(result => {
+            val typeRef = new TypeReference[List[Henkiloviite]] {}
+            val parsed = mapper.readValue(result, typeRef).toSet
+            LOG.info(s"Tiedot oppijanumerorekisteristä haettu, erä ${batch._2 + 1}/${batches.size}, henkiloviitteet: ${parsed.size}")
+            parsed
+          })
       batchResult
     })
     Future.sequence(allResults).map(_.flatten.toSet)
@@ -81,4 +112,5 @@ class OnrClientImpl(casClient: CasClient, environmentBaseUrl: String) extends On
         Future.failed(e)
     }
   }
+
 }
