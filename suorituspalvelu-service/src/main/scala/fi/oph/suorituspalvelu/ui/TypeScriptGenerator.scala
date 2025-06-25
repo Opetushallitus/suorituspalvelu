734f3c09f1e686cdcf780b727ab86cb0adb68f22
Santeri Korri <santeri.korri@knowit.fi>
2025-06-25 10:38:14 +0300

    OPHSUPA-34 Generoidaan success ja failure -tstyypit kaikille ui-endpointeille

--- suorituspalvelu-service/src/main/scala/fi/oph/suorituspalvelu/ui/TypeScriptGenerator.scala
+++ suorituspalvelu-service/src/main/scala/fi/oph/suorituspalvelu/ui/TypeScriptGenerator.scala
@@ -2,7 +2,7 @@ package fi.oph.suorituspalvelu.ui
 
 import com.scalatsi.*
 import com.scalatsi.output.{OutputOptions, StyleOptions, WriteTSToFiles}
-import fi.oph.suorituspalvelu.resource.ui.{AikuistenPerusopetuksenOppimaara, AmmatillinenTutkinto, AmmatillisenTutkinnonOsa, DIATutkinto, DIAVastaavuusTodistus, EBSuoritus, EBTutkinto, IBTutkinto, LukionOppimaara, NuortenPerusopetuksenOppiaineenOppimaara, OppijanTiedotSuccessResponse, OppimaaranOppiaine, PerusopetuksenOppiaine, PerusopetuksenOppiaineenOppimaara, PerusopetuksenOppimaara, PerusopetuksenOppimaara78Luokkalaiset, PreIB, VapaanSivistysTyonKoulutus, YOTutkinto}
+import fi.oph.suorituspalvelu.resource.ui.{AikuistenPerusopetuksenOppimaara, AmmatillinenTutkinto, AmmatillisenTutkinnonOsa, DIATutkinto, DIAVastaavuusTodistus, EBSuoritus, EBTutkinto, IBTutkinto, LukionOppimaara, NuortenPerusopetuksenOppiaineenOppimaara, OppijanHakuFailureResponse, OppijanHakuSuccessResponse, OppijanTiedotFailureResponse, OppijanTiedotSuccessResponse, OppilaitosFailureResponse, OppilaitosSuccessResponse, OppimaaranOppiaine, PerusopetuksenOppiaine, PerusopetuksenOppiaineenOppimaara, PerusopetuksenOppimaara, PerusopetuksenOppimaara78Luokkalaiset, PreIB, VapaanSivistysTyonKoulutus, YOTutkinto}
 
 import java.io.File
 import java.time.LocalDate
@@ -32,12 +32,22 @@ object TypeScriptGenerator extends App {
   implicit val perusopetuksenOppimaaraTSType: TSType[PerusopetuksenOppimaara] = TSType.fromCaseClass
   implicit val nuortenPerusopetuksenOppiaineenOppimaaraTSType: TSType[NuortenPerusopetuksenOppiaineenOppimaara] = TSType.fromCaseClass
   implicit val ammatillinenTutkintoTSType: TSType[AmmatillinenTutkinto] = TSType.fromCaseClass
-  implicit val oppijanTiedotTSType: TSType[OppijanTiedotSuccessResponse] = TSType.fromCaseClass
+  implicit val oppijanTiedotSuccessTSType: TSType[OppijanTiedotSuccessResponse] = TSType.fromCaseClass
+  implicit val oppijanTiedotFailureTSType: TSType[OppijanTiedotFailureResponse] = TSType.fromCaseClass
+  implicit val oppijatSuccessTSType: TSType[OppijanHakuSuccessResponse] = TSType.fromCaseClass
+  implicit val oppijatFailureTSType: TSType[OppijanHakuFailureResponse] = TSType.fromCaseClass
+  implicit val oppilaitosSuccessTSType: TSType[OppilaitosSuccessResponse] = TSType.fromCaseClass
+  implicit val oppilaitosFailureTSType: TSType[OppilaitosFailureResponse] = TSType.fromCaseClass
 
   // Kirjoitetaan TS-tyypit tiedostoon, polku kannattaa muuttaa sopivammaksi kun fronttityö etenee
   val outputDir = new File("target/generated-sources/typescript/Interface.ts")
   WriteTSToFiles.generate(OutputOptions(outputDir, StyleOptions()))(Map(
-    "oppijanTiedot" -> oppijanTiedotTSType.get
+    "oppijanTiedotSuccess" -> oppijanTiedotSuccessTSType.get,
+    "oppijanTiedotFailure" -> oppijanTiedotFailureTSType.get,
+    "oppijaHakuSuccess" -> oppijatSuccessTSType.get,
+    "oppijaHakuFailure" -> oppijatFailureTSType.get,
+    "oppilaitosSuccess" -> oppilaitosSuccessTSType.get,
+    "oppilaitosFailure" -> oppilaitosFailureTSType.get,
   ))
 
   println(s"TypeScript interfaces generated")
