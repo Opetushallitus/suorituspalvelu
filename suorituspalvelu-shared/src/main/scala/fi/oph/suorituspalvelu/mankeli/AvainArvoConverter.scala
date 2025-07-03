4c5ff50aea5307454b110dd9a3684b4eab77e4e2
Mikko Siukola <mikko.siukola@cybercom.com>
2025-07-03 16:52:22 +0300

    OPHSUPA-32 Renamings and tweaks

--- suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/mankeli/AvainArvoConverter.scala
+++ suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/mankeli/AvainArvoConverter.scala
@@ -4,9 +4,7 @@ import fi.oph.suorituspalvelu.business
 import fi.oph.suorituspalvelu.business.{NuortenPerusopetuksenOppiaineenOppimaara, Opiskeluoikeus, PerusopetuksenOpiskeluoikeus, PerusopetuksenOppimaara, Tietolahde}
 import org.slf4j.LoggerFactory
 
-trait MankeloituSuoritus
-
-case class ValintaData(keyValues: Map[String, String])
+case class ValintaData(personOid: String, hakemusOid: Option[String], keyValues: Map[String, String])
 
 object AvainArvoConstants {
   //Sama tieto tallennetaan kahden avaimen alle: vanhan Valintalaskentakoostepalvelusta periytyvän,
@@ -20,46 +18,45 @@ object AvainArvoConstants {
   final val peruskouluAineenArvosanaPrefixes = Set("PK_", "PERUSKOULU_ARVOSANA_")
 }
 
-object AvainArvoSovitin {
+object AvainArvoConverter {
 
   val LOG = LoggerFactory.getLogger(getClass)
-  
+
   //Ottaa huomioon ensi vaiheessa PerusopetuksenOppimaarat ja myöhemmin myös PerusopetuksenOppiaineenOppimaarien sisältämät arvosanat JOS löytyy suoritettu PerusopetuksenOppimaara
   def korkeimmatPerusopetuksenArvosanatAineittain(perusopetuksenOppimaara: Option[PerusopetuksenOppimaara], oppiaineenOppimaarat: Seq[NuortenPerusopetuksenOppiaineenOppimaara]): Set[(String, String)] = {
-    val arvosanat: Set[(String, String)] = perusopetuksenOppimaara.map(s => {
+    val oppimaaranArvosanat: Set[(String, String)] = perusopetuksenOppimaara.map(s => {
       if (s.vahvistusPaivamaara.isDefined) {
         s.aineet.flatMap(aine => AvainArvoConstants.peruskouluAineenArvosanaPrefixes.map(prefix => (prefix + aine.koodi.arvo, aine.arvosana.arvo)))
       } else Set.empty
     }).getOrElse(Set.empty)
-    arvosanat
+    oppimaaranArvosanat
   }
 
 
-  def mankeloiPeruskouluarvot(personOid: String, opiskeluoikeudet: Seq[Opiskeluoikeus]): ValintaData = {
+  def convertPeruskouluArvot(personOid: String, opiskeluoikeudet: Seq[Opiskeluoikeus]): ValintaData = {
     val perusopetukset: Seq[PerusopetuksenOpiskeluoikeus] = opiskeluoikeudet.collect { case po: PerusopetuksenOpiskeluoikeus => po }
     val (vahvistetut, eiVahvistetut) = perusopetukset.flatMap(po => po.suoritukset.find(_.isInstanceOf[PerusopetuksenOppimaara]).map(_.asInstanceOf[PerusopetuksenOppimaara])).partition(o => o.vahvistusPaivamaara.isDefined)
 
-    //Todo, tässä voi harkita myös virheen heittämistä
+    //Todo, tässä voi harkita myös virheen heittämistä. Ei liene järkevä tilanne, että vahvistettuja perusopetuksen oppimääriä olisi koskaan useita.
     if (vahvistetut.size > 1) {
       LOG.warn(s"Oppijalle $personOid enemmän kuin yksi vahvistettu perusopetuksen oppimäärä!")
     }
 
     val valmisOppimaara: Option[PerusopetuksenOppimaara] = vahvistetut.headOption
-    val keskenOppimaara: Option[PerusopetuksenOppimaara] = perusopetukset.flatMap(po => po.suoritukset.find(_.isInstanceOf[PerusopetuksenOppimaara]).map(_.asInstanceOf[PerusopetuksenOppimaara])).headOption
+    val keskenOppimaara: Option[PerusopetuksenOppimaara] = eiVahvistetut.headOption //Todo, tämän käsittelyyn tarvitaan jotain päivämäärätietoa suorituksille, jotta voidaan poimia tuorein. Periaatteessa näitä voisi olla useita.
 
-    val usePoo = valmisOppimaara.orElse(keskenOppimaara)
+    val useOppimaara = valmisOppimaara.orElse(keskenOppimaara)
 
-    val kieliArvot: Set[(String, String)] = usePoo.map(_.suoritusKieli.arvo).map(kieli => AvainArvoConstants.perusopetuksenKieliKeys.map(key => (key, kieli))).getOrElse(Set.empty)
+    val kieliArvot: Set[(String, String)] = useOppimaara.map(_.suoritusKieli.arvo).map(kieli => AvainArvoConstants.perusopetuksenKieliKeys.map(key => (key, kieli))).getOrElse(Set.empty)
 
-    val arvosanat = korkeimmatPerusopetuksenArvosanatAineittain(valmisOppimaara, Seq.empty)
+    val arvosanat: Set[(String, String)] = korkeimmatPerusopetuksenArvosanatAineittain(valmisOppimaara, Seq.empty)
 
     //Nämä poimitaan vain valmiilta suorituksilta
     val suoritusvuosi: Set[(String, String)] = AvainArvoConstants.peruskouluSuoritusvuosiKeys.map(key => (key, "2025")) //Todo, get from vahvistus date?
     val suoritettu: Set[(String, String)] = AvainArvoConstants.peruskouluSuoritettuKeys.map(key => (key, valmisOppimaara.isDefined.toString))
 
     val combined = (arvosanat ++ kieliArvot ++ suoritusvuosi ++ suoritettu).toMap
-    println(s"Result: $combined")
-    ValintaData(combined)
+    ValintaData(personOid, None, combined)
   }
 
 }
--- suorituspalvelu-shared/src/test/scala/fi/oph/suorituspalvelu/business/valinnat/mankeli/AvainArvoConverterTest.scala
+++ suorituspalvelu-shared/src/test/scala/fi/oph/suorituspalvelu/business/valinnat/mankeli/AvainArvoConverterTest.scala
@@ -2,14 +2,14 @@ package fi.oph.suorituspalvelu.business.parsing.koski
 
 import fi.oph.suorituspalvelu.business.KantaOperaatiot.KantaEntiteetit.{AMMATILLINEN_OPISKELUOIKEUS, GENEERINEN_OPISKELUOIKEUS, PERUSOPETUKSEN_OPISKELUOIKEUS}
 import fi.oph.suorituspalvelu.business.{AmmatillinenOpiskeluoikeus, GeneerinenOpiskeluoikeus, KantaOperaatiot, Opiskeluoikeus, PerusopetuksenOpiskeluoikeus}
-import fi.oph.suorituspalvelu.mankeli.{AvainArvoSovitin, AvainArvoConstants}
+import fi.oph.suorituspalvelu.mankeli.{AvainArvoConverter, AvainArvoConstants}
 import fi.oph.suorituspalvelu.parsing.koski.{KoskiParser, KoskiToSuoritusConverter}
 import org.junit.jupiter.api.TestInstance.Lifecycle
 import org.junit.jupiter.api.{Assertions, BeforeAll, Test, TestInstance}
 
 @Test
 @TestInstance(Lifecycle.PER_CLASS)
-class AvainArvoSovitinTest {
+class AvainArvoConverterTest {
 
   @Test def testKoskiParsingForAmmatillistenOpiskeluoikeuksienSuoritukset(): Unit = {
     val fileName = "/1_2_246_562_98_69863082363.json"
@@ -23,12 +23,12 @@ class AvainArvoSovitinTest {
 
       Assertions.assertEquals(1, oikeudet.size)
 
-      val foo = AvainArvoSovitin.mankeloiPeruskouluarvot("1.2.246.562.98.69863082363", oos)
+      val converterResult = AvainArvoConverter.convertPeruskouluArvot("1.2.246.562.98.69863082363", oos)
 
-      Assertions.assertEquals(Some("9"), foo.keyValues.get(AvainArvoConstants.peruskouluAineenArvosanaPrefixes.head+"GE"))
-      Assertions.assertEquals(Some("FI"), foo.keyValues.get(AvainArvoConstants.perusopetuksenKieliKeys.head))
-      Assertions.assertEquals(Some("2025"), foo.keyValues.get(AvainArvoConstants.peruskouluSuoritusvuosiKeys.head))
-      Assertions.assertEquals(Some("true"), foo.keyValues.get(AvainArvoConstants.peruskouluSuoritettuKeys.head))
+      Assertions.assertEquals(Some("9"), converterResult.keyValues.get(AvainArvoConstants.peruskouluAineenArvosanaPrefixes.head+"GE"))
+      Assertions.assertEquals(Some("FI"), converterResult.keyValues.get(AvainArvoConstants.perusopetuksenKieliKeys.head))
+      Assertions.assertEquals(Some("2025"), converterResult.keyValues.get(AvainArvoConstants.peruskouluSuoritusvuosiKeys.head))
+      Assertions.assertEquals(Some("true"), converterResult.keyValues.get(AvainArvoConstants.peruskouluSuoritettuKeys.head))
     })
   }
 
