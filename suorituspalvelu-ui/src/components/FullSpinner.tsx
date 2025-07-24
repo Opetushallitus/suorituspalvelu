9010e39e552ef669124415eed0c24ff8882c2223
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-07-24 09:52:44 +0300

    OPHSUPA-53: Lisätty latausanimaatio

--- suorituspalvelu-ui/src/components/FullSpinner.tsx
+++ suorituspalvelu-ui/src/components/FullSpinner.tsx
@@ -1,18 +1,24 @@
-export const FullSpinner = ({ ariaLabel }: { ariaLabel?: string }) => (
-  <div
-    aria-label={ariaLabel}
-    style={{
-      position: 'relative',
-      left: '0',
-      top: '0',
-      minHeight: '150px',
-      maxHeight: '80vh',
-      width: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    Ladataan...
-  </div>
-);
+import { CircularProgress } from '@mui/material';
+import { useTranslate } from '@tolgee/react';
+
+export const FullSpinner = ({ ariaLabel }: { ariaLabel?: string }) => {
+  const { t } = useTranslate();
+  return (
+    <div
+      aria-label={ariaLabel}
+      style={{
+        position: 'relative',
+        left: '0',
+        top: '0',
+        minHeight: '150px',
+        maxHeight: '80vh',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <CircularProgress aria-label={ariaLabel ?? t('ladataan')} />
+    </div>
+  );
+};
