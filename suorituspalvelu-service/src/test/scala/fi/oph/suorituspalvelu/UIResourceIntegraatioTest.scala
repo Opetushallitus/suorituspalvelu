2e0d7b56feee636f02f039bf5eb7eb5b837ea39b
Santeri Korri <santeri.korri@knowit.fi>
2025-08-06 11:59:33 +0300

    OPHSUPA-59 Haetaan henkilön kaikkien aliasten suoritustiedot käyttöliittymälle

--- suorituspalvelu-service/src/test/scala/fi/oph/suorituspalvelu/UIResourceIntegraatioTest.scala
+++ suorituspalvelu-service/src/test/scala/fi/oph/suorituspalvelu/UIResourceIntegraatioTest.scala
@@ -2,6 +2,7 @@ package fi.oph.suorituspalvelu
 
 import fi.oph.suorituspalvelu.business.Tietolahde.KOSKI
 import fi.oph.suorituspalvelu.business.{AmmatillinenOpiskeluoikeus, AmmatillinenPerustutkinto, Koodi}
+import fi.oph.suorituspalvelu.integration.{OnrIntegration, PersonOidsWithAliases}
 import fi.oph.suorituspalvelu.parsing.koski.Kielistetty
 import fi.oph.suorituspalvelu.resource.ui.{Oppija, OppijanHakuFailureResponse, OppijanHakuSuccessResponse, OppijanTiedotFailureResponse, OppijanTiedotSuccessResponse, Oppilaitos, OppilaitosSuccessResponse}
 import fi.oph.suorituspalvelu.resource.ApiConstants
@@ -9,6 +10,8 @@ import fi.oph.suorituspalvelu.security.{AuditOperation, SecurityConstants}
 import fi.oph.suorituspalvelu.ui.UIService
 import fi.oph.suorituspalvelu.validation.Validator
 import org.junit.jupiter.api.*
+import org.mockito.Mockito
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.test.context.support.{WithAnonymousUser, WithMockUser}
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -17,6 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.nio.charset.Charset
 import java.time.LocalDate
 import java.util.{Optional, UUID}
+import scala.concurrent.Future
 
 /**
  * UI-apin integraatiotestit. Testeissä on pyritty kattamaan kaikkien endpointtien kaikki eri paluuarvoihin
@@ -24,6 +28,9 @@ import java.util.{Optional, UUID}
  * yksikkötasolla. Onnistuneiden kutsujen osalta validoidaan että kannan tila kutsun jälkeen vastaa oletusta.
  */
 class UIResourceIntegraatioTest extends BaseIntegraatioTesti {
+
+  @MockBean
+  val onrIntegration: OnrIntegration = null
 
   @WithAnonymousUser
   @Test def testHaeOppilaitoksetAnonymous(): Unit =
@@ -178,6 +185,9 @@ class UIResourceIntegraatioTest extends BaseIntegraatioTesti {
   @Test def testHaeOppijanTiedotNotFound(): Unit =
     val oppijaNumero = "1.2.246.562.24.21250967216"
 
+    // mockataan ONR-vastaus
+    Mockito.when(onrIntegration.getAliasesForPersonOids(Set(oppijaNumero))).thenReturn(Future.successful(PersonOidsWithAliases(Map(oppijaNumero -> Set(oppijaNumero)))))
+
     // suoritetaan kutsu ja parseroidaan vastaus
     val result = mvc.perform(MockMvcRequestBuilders
         .get(ApiConstants.UI_TIEDOT_PATH.replace(ApiConstants.UI_TIEDOT_OPPIJANUMERO_PARAM_PLACEHOLDER, oppijaNumero), ""))
@@ -201,6 +211,9 @@ class UIResourceIntegraatioTest extends BaseIntegraatioTesti {
     kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(koskiVersio.get, Set(
       AmmatillinenOpiskeluoikeus(UUID.randomUUID(), "1.2.3", fi.oph.suorituspalvelu.business.Oppilaitos(Kielistetty(None, None, None), "1.2.3.4"), Set(ammatillinenTutkinto), None),
     ), Set.empty)
+
+    // mockataan ONR-vastaus
+    Mockito.when(onrIntegration.getAliasesForPersonOids(Set(oppijaNumero))).thenReturn(Future.successful(PersonOidsWithAliases(Map(oppijaNumero -> Set(oppijaNumero)))))
 
     // suoritetaan kutsu ja parseroidaan vastaus
     val result = mvc.perform(MockMvcRequestBuilders
