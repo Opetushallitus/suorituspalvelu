ec45f384a04d501e20556018ced7b5d2cb0bf1e2
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-08-07 13:18:43 +0300

    OPHSUPA-57: Lisätty YO-kokeet-taulukko oppijan suoritustietoihin

--- suorituspalvelu-ui/src/components/LukioSuoritusPaper.tsx
+++ suorituspalvelu-ui/src/components/LukioSuoritusPaper.tsx
@@ -1,10 +1,13 @@
-import { LukionOppiaine, LukioSuoritus } from '@/types/ui-types';
+import { LukionOppiaine, LukioSuoritus, YOKoe } from '@/types/ui-types';
 import { SuoritusInfoPaper } from './SuoritusInfoPaper';
 import { ophColors } from '@opetushallitus/oph-design-system';
 import { SuorituksenPerustiedotIndicator } from './SuorituksenPerustiedotIndicator';
 import { useTranslate } from '@tolgee/react';
 import { LabeledInfoItem } from './LabeledInfoItem';
 import { styled } from '@/lib/theme';
+import { ListTable, ListTableColumn } from './ListTable';
+import { formatDate } from 'date-fns';
+import { useMemo } from 'react';
 
 const UnorderedList = styled('ul')(({ theme }) => ({
   margin: 0,
@@ -31,6 +34,43 @@ const LukionOppiaineetList = ({
   );
 };
 
+const YoKokeetTable = ({ yoKokeet }: { yoKokeet: Array<YOKoe> }) => {
+  const { t } = useTranslate();
+
+  const cols: Array<ListTableColumn<YOKoe>> = useMemo(
+    () => [
+      {
+        key: 'aine',
+        title: t('oppija.yo-kokeen-aine'),
+        render: (row) => row.aine,
+      },
+      {
+        key: 'taso',
+        title: t('oppija.yo-kokeen-taso'),
+        render: (row) => row.taso,
+      },
+      {
+        key: 'arvosana',
+        title: t('oppija.yo-kokeen-arvosana'),
+        render: (row) => row.arvosana,
+      },
+      {
+        key: 'yhteispistemaara',
+        title: t('oppija.yo-kokeen-yhteispistemaara'),
+        render: (row) => row.yhteispistemaara.toString(),
+      },
+      {
+        key: 'tutkintokerta',
+        title: t('oppija.yo-kokeen-tutkintokerta'),
+        render: (row) => formatDate(row.tutkintokerta, 'd.M.y'),
+      },
+    ],
+    [t],
+  );
+
+  return <ListTable columns={cols} rows={yoKokeet} rowKeyProp="aine" />;
+};
+
 export const LukioSuoritusPaper = ({
   suoritus,
 }: {
@@ -46,6 +86,7 @@ export const LukioSuoritusPaper = ({
       {'oppiaineet' in suoritus && (
         <LukionOppiaineetList oppiaineet={suoritus.oppiaineet} />
       )}
+      {'yoKokeet' in suoritus && <YoKokeetTable yoKokeet={suoritus.yoKokeet} />}
     </SuoritusInfoPaper>
   );
 };
