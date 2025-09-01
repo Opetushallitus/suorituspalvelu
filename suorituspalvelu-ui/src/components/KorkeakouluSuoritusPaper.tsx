3197bd875c2d89c6c4cb455b605a62f374f694f3
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-09-01 15:06:27 +0300

    Poistettu käyttöliittymästä korkeakoulusuoritukselta hakukohde

--- suorituspalvelu-ui/src/components/KorkeakouluSuoritusPaper.tsx
+++ suorituspalvelu-ui/src/components/KorkeakouluSuoritusPaper.tsx
@@ -1,8 +1,6 @@
 import { ophColors } from '@opetushallitus/oph-design-system';
 import { SuoritusInfoPaper } from './SuoritusInfoPaper';
 import { SuorituksenPerustiedotIndicator } from './SuorituksenPerustiedotIndicator';
-import { LabeledInfoItem } from './LabeledInfoItem';
-import { useTranslations } from '@/hooks/useTranslations';
 import { KorkeakouluSuoritus } from '@/types/ui-types';
 
 export const KorkeakouluSuoritusPaper = ({
@@ -10,16 +8,10 @@ export const KorkeakouluSuoritusPaper = ({
 }: {
   suoritus?: KorkeakouluSuoritus | undefined;
 }) => {
-  const { t, translateKielistetty } = useTranslations();
-
   return (
     suoritus && (
       <SuoritusInfoPaper suoritus={suoritus} topColor={ophColors.red1}>
         <SuorituksenPerustiedotIndicator perustiedot={suoritus} />
-        <LabeledInfoItem
-          label={t('oppija.hakukohde')}
-          value={translateKielistetty(suoritus.hakukohde.nimi)}
-        />
       </SuoritusInfoPaper>
     )
   );
