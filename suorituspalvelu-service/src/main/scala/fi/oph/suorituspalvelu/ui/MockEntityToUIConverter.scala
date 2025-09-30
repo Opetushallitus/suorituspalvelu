285b67a7ca242f1652fc186397014bc7b0aee898
Santeri Korri <santeri.korri@knowit.fi>
2025-09-30 11:39:26 +0300

    OPHSUPA-92 Muutettu perusopetuksen oppimäärän yksilöllistäminen optionaaliseksi

--- suorituspalvelu-service/src/main/scala/fi/oph/suorituspalvelu/ui/MockEntityToUIConverter.scala
+++ suorituspalvelu-service/src/main/scala/fi/oph/suorituspalvelu/ui/MockEntityToUIConverter.scala
@@ -822,11 +822,11 @@ object MockEntityToUIConverter {
       valmistumispaiva = Optional.of(LocalDate.parse("2016-06-01")),
       suorituskieli = "suomi",
       luokka = Optional.of("9A"),
-      yksilollistaminen = Yksilollistaminen(2, YksilollistamisNimi(
+      yksilollistaminen = Optional.of(Yksilollistaminen(2, YksilollistamisNimi(
         Optional.of("Perusopetuksen osittain yksilöllistetty oppimäärä"),
         Optional.of("Delvis individualiserad lärokurs inom den grundläggande utbildningen"),
         Optional.of("Partially individualized basic education syllabus")
-      )),
+      ))),
       oppiaineet = java.util.List.of(
         PerusopetuksenOppiaine(
           tunniste = UUID.randomUUID(),
