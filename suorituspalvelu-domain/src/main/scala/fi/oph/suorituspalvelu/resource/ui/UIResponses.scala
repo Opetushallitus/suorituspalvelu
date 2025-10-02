cd4e49f06ecbe81933abe319149849be7b21a12e
Santeri Korri <santeri.korri@knowit.fi>
2025-10-02 12:30:38 +0300

    OPHSUPA-72 Lisätty arvosanat dropdown-dataan

--- suorituspalvelu-domain/src/main/scala/fi/oph/suorituspalvelu/resource/ui/UIResponses.scala
+++ suorituspalvelu-domain/src/main/scala/fi/oph/suorituspalvelu/resource/ui/UIResponses.scala
@@ -1240,6 +1240,11 @@ case class SyotettavaYksilollistamisVaihtoehto(
   @BeanProperty arvo: Int
 )
 
+case class SyotettavaArvosanaVaihtoehto(
+  @(Schema @field)(example = "10", requiredMode = RequiredMode.REQUIRED)
+  @BeanProperty arvo: Int
+)
+
 trait LuoSuoritusDropdownDataResponse()
 
 case class LuoSuoritusDropdownDataSuccessResponse(
@@ -1255,6 +1260,8 @@ case class LuoSuoritusDropdownDataSuccessResponse(
   @BeanProperty vieraatKielet: java.util.List[SyotettavaVierasKieliVaihtoehto],
   @(Schema @field)(requiredMode = RequiredMode.REQUIRED)
   @BeanProperty yksilollistaminen: java.util.List[SyotettavaYksilollistamisVaihtoehto],
+  @(Schema @field)(requiredMode = RequiredMode.REQUIRED)
+  @BeanProperty arvosanat: java.util.List[SyotettavaArvosanaVaihtoehto],
 ) extends LuoSuoritusDropdownDataResponse
 
 case class LuoSuoritusDropdownDataFailureResponse(
