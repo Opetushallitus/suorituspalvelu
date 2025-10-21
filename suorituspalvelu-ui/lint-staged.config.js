f55db19c0ea73ae7a12963f960eb6c86e8cf04af
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-10-21 12:35:36 +0300

    Korjattu lint-staged "konfiguraatiota ei löydy"-ongelma

--- suorituspalvelu-ui/lint-staged.config.js
+++ suorituspalvelu-ui/lint-staged.config.js
@@ -1,9 +1,12 @@
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+
 const eslintCommand = 'eslint --fix --no-warn-ignored --max-warnings=0';
 const prettierCommand = 'prettier --write -u';
 
-const config = {
+export default {
   '**/*.{js,mjs,cjs,jsx,ts,tsx}': [eslintCommand, prettierCommand],
   '!**/*.{js,mjs,cjs,jsx,ts,tsx}': prettierCommand,
 };
-
-export default config;
