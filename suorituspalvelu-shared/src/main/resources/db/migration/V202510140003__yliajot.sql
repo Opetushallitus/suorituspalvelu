e6c9bfa74f3e99fc6c7252fe590201132229124f
Mikko Siukola <mikko.siukola@cybercom.com>
2025-10-22 16:48:23 +0300

    OPHSUPA-103 Improve yliajot table, update migration

--- suorituspalvelu-shared/src/main/resources/db/migration/V202510140003__yliajot.sql
+++ suorituspalvelu-shared/src/main/resources/db/migration/V202510140003__yliajot.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS yliajot (
     haku_oid       VARCHAR,
     virkailija_oid VARCHAR,
     selite         VARCHAR,
-    voimassaolo    TSTZRANGE NOT NULL
+    voimassaolo    TSTZRANGE NOT NULL,
+    EXCLUDE USING gist (henkilo_oid WITH =, haku_oid WITH =, avain WITH =, voimassaolo WITH &&)
 );
 
-CREATE INDEX IF NOT EXISTS execution_time_idx ON yliajot (henkilo_oid, haku_oid);
+CREATE INDEX IF NOT EXISTS yliajot_henkilo_haku_idx ON yliajot (henkilo_oid, haku_oid);
