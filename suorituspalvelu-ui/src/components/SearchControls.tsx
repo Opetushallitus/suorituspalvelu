13ac0841d4bc094be58c2dfa6f4c624c7e740e9d
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-11-06 09:44:35 +0200

    OPHSUPA-128: Poistettu oppilaitos-pudotusvalikko henkilöhausta

--- suorituspalvelu-ui/src/components/SearchControls.tsx
+++ suorituspalvelu-ui/src/components/SearchControls.tsx
@@ -1,42 +1,13 @@
 import { useOppijatSearchParamsState } from '@/hooks/useSearchOppijat';
 import { DEFAULT_BOX_BORDER } from '@/lib/theme';
-import { useOppilaitoksetOptions } from '@/lib/suorituspalvelu-queries';
 import { Stack } from '@mui/material';
-import {
-  OphInputFormField,
-  OphSelectFormField,
-} from '@opetushallitus/oph-design-system';
+import { OphInputFormField } from '@opetushallitus/oph-design-system';
 import { useTranslations } from '@/hooks/useTranslations';
-
-const OppilaitosSelectField = ({
-  value,
-  onChange,
-}: Pick<
-  React.ComponentProps<typeof OphSelectFormField>,
-  'value' | 'onChange'
->) => {
-  const { t } = useTranslations();
-
-  const oppilaitoksetOptions = useOppilaitoksetOptions();
-
-  return (
-    <OphSelectFormField
-      sx={{ minWidth: '200px' }}
-      options={oppilaitoksetOptions}
-      clearable={true}
-      placeholder={t('select.valitse')}
-      value={value}
-      label={t('oppilaitos')}
-      onChange={onChange}
-    />
-  );
-};
 
 export function SearchControls() {
   const { t } = useTranslations();
 
-  const { tunniste, setTunniste, oppilaitos, setOppilaitos } =
-    useOppijatSearchParamsState();
+  const { tunniste, setTunniste } = useOppijatSearchParamsState();
 
   return (
     <Stack
@@ -56,12 +27,6 @@ export function SearchControls() {
         value={tunniste ?? ''}
         onChange={(e) => {
           setTunniste(e.target.value);
-        }}
-      />
-      <OppilaitosSelectField
-        value={oppilaitos ?? ''}
-        onChange={(e) => {
-          setOppilaitos(e.target.value);
         }}
       />
     </Stack>
