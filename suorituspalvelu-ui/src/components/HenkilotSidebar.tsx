1df07dfd70b6e08223a7e33a4f247ef8fba15def
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-11-26 17:15:02 +0200

    OPHSUPA-114: Parannettu tarkistusnäkymän toimintalogiikkaa
    - Järjestetään valittavissa olevat oppilaitokset, vuodet ja luokat
    - Valitaan oppilaitos automaattisesti, jos vain yksi valittavissa
    - Valitaan valmistumisvuodeksi automaattisesti kuluva vuosi
    - Näytetään sivupalkissa kehotus valita oppilaitos, jos ei valittu

--- suorituspalvelu-ui/src/components/HenkilotSidebar.tsx
+++ suorituspalvelu-ui/src/components/HenkilotSidebar.tsx
@@ -17,9 +17,9 @@ import { Box, Stack } from '@mui/material';
 
 const HenkilotSidebarContent = () => {
   const params = useOppijatSearchURLParams();
-  const { data } = useOppilaitoksenOppijatSearch();
+  const { data, totalCount } = useOppilaitoksenOppijatSearch();
 
-  const { setSearchParams, hasEmptySearchParams } =
+  const { setSearchParams, hasValidSearchParams } =
     useOppijatSearchParamsState();
 
   const { t } = useTranslate();
@@ -28,48 +28,59 @@ const HenkilotSidebarContent = () => {
 
   const searchTab = useSelectedSearchTab();
 
-  return hasEmptySearchParams ? (
-    <div></div>
-  ) : (
+  return (
     <Stack spacing={1.5} sx={{ paddingLeft: 2 }}>
-      <Box sx={{ paddingRight: 2, paddingTop: 1 }}>
-        <SearchInput
-          sx={{ width: '100%' }}
-          value={params.tunniste ?? ''}
-          onClear={() => {
-            setSearchParams({ tunniste: '' });
-          }}
-          onChange={(e) => {
-            setSearchParams({ tunniste: e.target.value });
-          }}
-        />
-      </Box>
-      <OphTypography variant="body2">
-        {t('sivupalkki.henkilo-maara', {
-          count: data?.length ?? 0,
-        })}
-      </OphTypography>
-      <NavigationList tabIndex={0} aria-label={t('sivupalkki.navigaatio')}>
-        {data?.map((oppija) => (
-          <Link
-            key={oppija.oppijaNumero}
-            className={
-              oppijaNumero === oppija.oppijaNumero
-                ? NAV_LIST_SELECTED_ITEM_CLASS
-                : ''
-            }
-            to={{
-              pathname: `/${searchTab}/${oppija.oppijaNumero}`,
-              search: new URLSearchParams(params).toString(),
-            }}
-          >
-            <OphTypography variant="label" color="inherit">
-              {formatHenkiloNimi(oppija, t)}
-            </OphTypography>
-            <OphTypography color={ophColors.black}>{oppija.hetu}</OphTypography>
-          </Link>
-        ))}
-      </NavigationList>
+      {hasValidSearchParams ? (
+        <>
+          {totalCount !== 0 && (
+            <Box sx={{ paddingRight: 2, paddingTop: 1 }}>
+              <SearchInput
+                sx={{ width: '100%' }}
+                placeholder={t('sivupalkki.suodata-nimella-tai-hetulla')}
+                value={params.tunniste ?? ''}
+                onClear={() => {
+                  setSearchParams({ tunniste: '' });
+                }}
+                onChange={(e) => {
+                  setSearchParams({ tunniste: e.target.value });
+                }}
+              />
+            </Box>
+          )}
+          <OphTypography variant="body2">
+            {t('sivupalkki.henkilo-maara', {
+              count: data?.length ?? 0,
+            })}
+          </OphTypography>
+          <NavigationList tabIndex={0} aria-label={t('sivupalkki.navigaatio')}>
+            {data?.map((oppija) => (
+              <Link
+                key={oppija.oppijaNumero}
+                className={
+                  oppijaNumero === oppija.oppijaNumero
+                    ? NAV_LIST_SELECTED_ITEM_CLASS
+                    : ''
+                }
+                to={{
+                  pathname: `/${searchTab}/${oppija.oppijaNumero}`,
+                  search: new URLSearchParams(params).toString(),
+                }}
+              >
+                <OphTypography variant="label" color="inherit">
+                  {formatHenkiloNimi(oppija, t)}
+                </OphTypography>
+                <OphTypography color={ophColors.black}>
+                  {oppija.hetu}
+                </OphTypography>
+              </Link>
+            ))}
+          </NavigationList>
+        </>
+      ) : (
+        <OphTypography variant="body2">
+          {t('search.valitse-oppilaitos')}
+        </OphTypography>
+      )}
     </Stack>
   );
 };
