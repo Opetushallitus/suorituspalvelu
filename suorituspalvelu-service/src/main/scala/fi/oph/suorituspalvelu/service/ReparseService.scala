6b55c8657f503fc14d6b90827650be1a8f085a6f
Santeri Korri <santeri.korri@knowit.fi>
2026-01-06 12:36:02 +0200

    OPHSUPA-160 Endpoint raakadatan uudelleenparserointiin

---
+++ suorituspalvelu-service/src/main/scala/fi/oph/suorituspalvelu/service/ReparseService.scala
@@ -0,0 +1,123 @@
+package fi.oph.suorituspalvelu.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import fi.oph.suorituspalvelu.business.{KantaOperaatiot, Opiskeluoikeus, SuoritusJoukko, VersioEntiteetti}
+import fi.oph.suorituspalvelu.jobs.SupaScheduler
+import fi.oph.suorituspalvelu.parsing.koski.{KoskiParser, KoskiToSuoritusConverter, KoskiUtil}
+import fi.oph.suorituspalvelu.parsing.virkailija.VirkailijaToSuoritusConverter
+import fi.oph.suorituspalvelu.parsing.virta.{VirtaParser, VirtaToSuoritusConverter}
+import fi.oph.suorituspalvelu.parsing.ytr.{YtrParser, YtrToSuoritusConverter}
+import fi.oph.suorituspalvelu.resource.ui.{SyotettyPerusopetuksenOppiaineenOppimaarienSuoritusContainer, SyotettyPerusopetuksenOppimaaranSuoritus}
+import fi.oph.suorituspalvelu.util.{KoodistoProvider, OrganisaatioProvider}
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+import java.time.{Duration, Instant}
+import java.util.UUID
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import scala.jdk.CollectionConverters.*
+
+@Service
+class ReparseService(scheduler: SupaScheduler, kantaOperaatiot: KantaOperaatiot, koodistoProvider: KoodistoProvider, organisaatioProvider: OrganisaatioProvider, objectMapper: ObjectMapper) {
+
+  val LOG = LoggerFactory.getLogger(classOf[ReparseService])
+
+  final val PROGRESS_UPDATE_INTERVAL = 100
+  final val TIMEOUT = 30.seconds
+
+  private val HENKILO_TIMEOUT = 5.minutes
+  
+  private val reparseKoskiJob = scheduler.registerJob("reparse-koski-data", (ctx, dryRun) => {
+    LOG.info(s"Uudelleenparseroidaan KOSKI-data, job id: ${ctx.getJobId}")
+    val versiot = kantaOperaatiot.haeVersiot(SuoritusJoukko.KOSKI)
+    versiot.zipWithIndex.foreach((versio, idx) => {
+        try
+          if(idx % PROGRESS_UPDATE_INTERVAL == 0) ctx.updateProgress(idx.toDouble/versiot.size.toDouble)
+          val (_, data) = kantaOperaatiot.haeData(versio)
+          val parsed = data.flatMap(d => KoskiParser.parseKoskiData(d))
+          val converted = KoskiToSuoritusConverter.parseOpiskeluoikeudet(parsed, koodistoProvider).toSet
+          if(!dryRun.toBoolean) kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, converted, KoskiUtil.getLahtokouluMetadata(converted))
+        catch
+          case e: Exception => LOG.error(s"Virhe henkilön ${versio.oppijaNumero} KOSKI-version ${versio.tunniste.toString} uudelleenparseroinnissa, job-id: ${ctx.getJobId}", e)
+      })
+    LOG.info(s"KOSKI-datan uudelleenparserpointi valmis, job id: ${ctx.getJobId}")
+  }, Seq.empty)
+
+  def reparseKoski(dryRun: Boolean): UUID = reparseKoskiJob.run(dryRun.toString)
+
+  private val reparseVirtaJob = scheduler.registerJob("reparse-virta-data", (ctx, dryRun) => {
+    LOG.info(s"Uudelleenparseroidaan VIRTA-data, job id: ${ctx.getJobId}")
+    val versiot = kantaOperaatiot.haeVersiot(SuoritusJoukko.VIRTA)
+    versiot.zipWithIndex.foreach((versio, idx) => {
+        try
+          if(idx % PROGRESS_UPDATE_INTERVAL == 0) ctx.updateProgress(idx.toDouble/versiot.size.toDouble)
+          val (_, data) = kantaOperaatiot.haeData(versio)
+          val parsed = data.map(d => VirtaParser.parseVirtaData(d))
+          val converted: Set[Opiskeluoikeus] = parsed.flatMap(p => VirtaToSuoritusConverter.toOpiskeluoikeudet(p)).toSet
+          if(!dryRun.toBoolean) kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, converted, KoskiUtil.getLahtokouluMetadata(converted))
+        catch
+          case e: Exception => LOG.error(s"Virhe henkilön ${versio.oppijaNumero} VIRTA-version ${versio.tunniste.toString} uudelleenparseroinnissa, job-id: ${ctx.getJobId}", e)
+      })
+    LOG.info(s"VIRTA-datan uudelleenparserpointi valmis, job id: ${ctx.getJobId}")
+  }, Seq.empty)
+
+  def reparseVirta(dryRun: Boolean): UUID = reparseVirtaJob.run(dryRun.toString)
+
+  private val reparseYTRJob = scheduler.registerJob("reparse-ytr-data", (ctx, dryRun) => {
+    LOG.info(s"Uudelleenparseroidaan YTR-data, job id: ${ctx.getJobId}")
+    val versiot = kantaOperaatiot.haeVersiot(SuoritusJoukko.YTR)
+    versiot.zipWithIndex.foreach((versio, idx) => {
+        try
+          if(idx % PROGRESS_UPDATE_INTERVAL == 0) ctx.updateProgress(idx.toDouble/versiot.size.toDouble)
+          val (_, data) = kantaOperaatiot.haeData(versio)
+          val parsed = data.map(d => YtrParser.parseYtrData(d))
+          val converted: Set[Opiskeluoikeus] = parsed.map(s => YtrToSuoritusConverter.toSuoritus(s)).toSet
+          if(!dryRun.toBoolean) kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, converted, KoskiUtil.getLahtokouluMetadata(converted))
+        catch
+          case e: Exception => LOG.error(s"Virhe henkilön ${versio.oppijaNumero} YTR-version ${versio.tunniste.toString} uudelleenparseroinnissa, job-id: ${ctx.getJobId}", e)
+      })
+    LOG.info(s"YTR-datan uudelleenparserpointi valmis, job id: ${ctx.getJobId}")
+  }, Seq.empty)
+
+  def reparseYTR(dryRun: Boolean): UUID = reparseYTRJob.run(dryRun.toString)
+
+  private val reparsePerusopetuksenOppimaaratJob = scheduler.registerJob("reparse-perusopetus-data", (ctx, dryRun) => {
+    LOG.info(s"Uudelleenparseroidaan syötetyt perusopetuksen oppimäärät, job id: ${ctx.getJobId}")
+    val versiot = kantaOperaatiot.haeVersiot(SuoritusJoukko.SYOTETTY_PERUSOPETUS)
+    versiot.zipWithIndex.foreach((versio, idx) => {
+        try
+          if(idx % PROGRESS_UPDATE_INTERVAL == 0) ctx.updateProgress(idx.toDouble/versiot.size.toDouble)
+          val (_, data) = kantaOperaatiot.haeData(versio)
+          val parsed = data.map(d => objectMapper.readValue(d, classOf[SyotettyPerusopetuksenOppimaaranSuoritus]))
+          val converted: Set[Opiskeluoikeus] = parsed.map(p => VirkailijaToSuoritusConverter.toPerusopetuksenOppimaara(versio.tunniste, p, koodistoProvider, organisaatioProvider)).toSet
+          if(!dryRun.toBoolean) kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, converted, KoskiUtil.getLahtokouluMetadata(converted))
+        catch
+          case e: Exception => LOG.error(s"Virhe henkilön ${versio.oppijaNumero} YTR-version ${versio.tunniste.toString} uudelleenparseroinnissa, job-id: ${ctx.getJobId}", e)
+      })
+    LOG.info(s"Syotettyjen perusopetuksen oppimäärien uudelleenparserpointi valmis, job id: ${ctx.getJobId}")
+  }, Seq.empty)
+
+  def reparsePerusopetuksenOppimaarat(dryRun: Boolean): UUID = reparsePerusopetuksenOppimaaratJob.run(dryRun.toString)
+
+  private val reparsePerusopetuksenOppiaineenOppimaaratJob = scheduler.registerJob("reparse-perusopetus-oppiaineet-data", (ctx, dryRun) => {
+    LOG.info(s"Uudelleenparseroidaan syötetyt perusopetuksen oppimäärän oppiaineet, job id: ${ctx.getJobId}")
+    val versiot = kantaOperaatiot.haeVersiot(SuoritusJoukko.SYOTETYT_OPPIAINEET)
+    versiot.zipWithIndex.foreach((versio, idx) => {
+        try
+          if(idx % PROGRESS_UPDATE_INTERVAL == 0) ctx.updateProgress(idx.toDouble/versiot.size.toDouble)
+          val (_, data) = kantaOperaatiot.haeData(versio)
+          val parsed = data.map(d => objectMapper.readValue(d, classOf[SyotettyPerusopetuksenOppiaineenOppimaarienSuoritusContainer]))
+          val converted: Set[Opiskeluoikeus] = parsed.map(p => VirkailijaToSuoritusConverter.toPerusopetuksenOppiaineenOppimaara(versio.tunniste, p, koodistoProvider, organisaatioProvider)).toSet
+          if(!dryRun.toBoolean) kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, converted, KoskiUtil.getLahtokouluMetadata(converted))
+        catch
+          case e: Exception => LOG.error(s"Virhe henkilön ${versio.oppijaNumero} YTR-version ${versio.tunniste.toString} uudelleenparseroinnissa, job-id: ${ctx.getJobId}", e)
+      })
+    LOG.info(s"Syotettyjen perusopetuksen oppiaineen oppimäärien uudelleenparserpointi valmis, job id: ${ctx.getJobId}")
+  }, Seq.empty)
+
+  def reparsePerusopetuksenOppiaineenOppimaarat(dryRun: Boolean): UUID = reparsePerusopetuksenOppiaineenOppimaaratJob.run(dryRun.toString)
+  
+}
---
+++ suorituspalvelu-service/src/test/scala/fi/oph/suorituspalvelu/ReparseIntegraatioTest.scala
@@ -0,0 +1,217 @@
+package fi.oph.suorituspalvelu
+
+import com.fasterxml.jackson.core.`type`.TypeReference
+import fi.oph.suorituspalvelu.business.KKOpiskeluoikeusTila.VOIMASSA
+import fi.oph.suorituspalvelu.business.SuoritusTila.VALMIS
+import fi.oph.suorituspalvelu.business.{Koodi, Opiskeluoikeus, PerusopetuksenOpiskeluoikeus, PerusopetuksenOppimaara, PerusopetuksenVuosiluokka, SuoritusJoukko, VirtaOpiskeluoikeus, YOOpiskeluoikeus, YOTutkinto}
+import fi.oph.suorituspalvelu.parsing.koski.{Kielistetty, KoskiUtil}
+import fi.oph.suorituspalvelu.resource.ApiConstants
+import fi.oph.suorituspalvelu.resource.api.*
+import fi.oph.suorituspalvelu.security.{AuditOperation, SecurityConstants}
+import org.junit.jupiter.api.TestInstance.Lifecycle
+import org.junit.jupiter.api.{Assertions, Test, TestInstance}
+import org.springframework.security.test.context.support.{WithAnonymousUser, WithMockUser}
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+import java.nio.charset.Charset
+import java.time.{Instant, LocalDate}
+import java.util.UUID
+import scala.jdk.CollectionConverters.*
+
+@Test
+@TestInstance(Lifecycle.PER_CLASS)
+class ReparseIntegraatioTest extends BaseIntegraatioTesti {
+
+  // -- Uudelleenparserointi endpoint tests --
+
+  @WithAnonymousUser
+  @Test def testUudelleenParseroiAnonymous(): Unit =
+    // tuntematon käyttäjä ohjataan tunnistautumiseen
+    mvc.perform(jsonPost(ApiConstants.DATASYNC_UUDELLEENPARSEROI_PATH, "payloadilla ei ole väliä"))
+      .andExpect(status().is3xxRedirection())
+
+  @WithMockUser(value = "kayttaja", authorities = Array())
+  @Test def testUudelleenParseroiNotAllowed(): Unit =
+    // tunnistettu käyttäjä jolla ei oikeuksia => 403
+    mvc.perform(jsonPost(ApiConstants.DATASYNC_UUDELLEENPARSEROI_PATH, "payloadilla ei ole väliä"))
+      .andExpect(status().isForbidden())
+
+  @WithMockUser(value = "kayttaja", authorities = Array(SecurityConstants.SECURITY_ROOLI_REKISTERINPITAJA_FULL))
+  @Test def testUudelleenParseroiMalformedJson(): Unit =
+    // ei validi JSON ei sallittu
+    val result = mvc.perform(jsonPost(ApiConstants.DATASYNC_UUDELLEENPARSEROI_PATH, "tämä ei ole validia jsonia"))
+      .andExpect(status().isBadRequest).andReturn()
+
+    Assertions.assertEquals(KoskiSyncFailureResponse(List(ApiConstants.DATASYNC_JSON_VIRHE).asJava),
+      objectMapper.readValue(result.getResponse.getContentAsString(Charset.forName("UTF-8")), classOf[KoskiSyncFailureResponse]))
+
+  @WithMockUser(value = "kayttaja", authorities = Array(SecurityConstants.SECURITY_ROOLI_REKISTERINPITAJA_FULL))
+  @Test def testUudelleenParseroiKoski(): Unit = {
+    // tallennetaan versio ja parseroitua dataa
+    val versio = kantaOperaatiot.tallennaJarjestelmaVersio(ApiConstants.ESIMERKKI_OPPIJANUMERO, SuoritusJoukko.KOSKI, Seq.empty, Instant.now()).get
+    val opiskeluoikeudet: Set[Opiskeluoikeus] = Set(PerusopetuksenOpiskeluoikeus(
+      UUID.randomUUID(),
+      None,
+      ApiConstants.ESIMERKKI_OPPILAITOS_OID,
+      Set.empty,
+      None,
+      VALMIS,
+      List.empty
+    ))
+    kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, opiskeluoikeudet, Seq.empty)
+    
+    // suoritetaan kutsu ja parseroidaan vastaus
+    val payload = new ReparsePayload().copy(koski = true)
+    val result = mvc.perform(jsonPost(ApiConstants.DATASYNC_UUDELLEENPARSEROI_PATH, payload))
+      .andExpect(status().isOk).andReturn()
+    val response = objectMapper.readValue(result.getResponse.getContentAsString(Charset.forName("UTF-8")), classOf[ReparseSuccessResponse])
+
+    // katsotaan että kutsun tiedot tallentuvat auditlokiin
+    val auditLogEntry = getLatestAuditLogEntry()
+    Assertions.assertEquals(AuditOperation.Uudelleenparseroi.name, auditLogEntry.operation)
+    val typeRef = new TypeReference[java.util.Map[String, String]] {}
+    Assertions.assertEquals(objectMapper.readValue(objectMapper.writeValueAsString(payload), typeRef).asScala.toMap, auditLogEntry.target)
+
+    // odotetaan että parserointi valmis
+    Assertions.assertEquals(1, response.jobids.size())
+    waitUntilReady(response.jobids.asScala.head)
+    
+    // uudelleenparserointi poistanut opiskeluoikeuden
+    Assertions.assertEquals(Map.empty, kantaOperaatiot.haeSuoritukset(ApiConstants.ESIMERKKI_OPPIJANUMERO))
+  }
+
+  @WithMockUser(value = "kayttaja", authorities = Array(SecurityConstants.SECURITY_ROOLI_REKISTERINPITAJA_FULL))
+  @Test def testUudelleenParseroiVirta(): Unit = {
+    // tallennetaan versio ja parseroitua dataa
+    val versio = kantaOperaatiot.tallennaJarjestelmaVersio(ApiConstants.ESIMERKKI_OPPIJANUMERO, SuoritusJoukko.VIRTA, Seq.empty, Instant.now()).get
+    val opiskeluoikeudet: Set[Opiskeluoikeus] = Set(VirtaOpiskeluoikeus(
+      UUID.randomUUID(),
+      "",
+      "",
+      LocalDate.now(),
+      LocalDate.now(),
+      Koodi("arvo", "koodisto", None),
+      VOIMASSA,
+      "",
+      Set.empty
+    ))
+    kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, opiskeluoikeudet, Seq.empty)
+
+    // suoritetaan kutsu ja parseroidaan vastaus
+    val payload = new ReparsePayload().copy(virta = true)
+    val result = mvc.perform(jsonPost(ApiConstants.DATASYNC_UUDELLEENPARSEROI_PATH, payload))
+      .andExpect(status().isOk).andReturn()
+    val response = objectMapper.readValue(result.getResponse.getContentAsString(Charset.forName("UTF-8")), classOf[ReparseSuccessResponse])
+
+    // katsotaan että kutsun tiedot tallentuvat auditlokiin
+    val auditLogEntry = getLatestAuditLogEntry()
+    Assertions.assertEquals(AuditOperation.Uudelleenparseroi.name, auditLogEntry.operation)
+    val typeRef = new TypeReference[java.util.Map[String, String]] {}
+    Assertions.assertEquals(objectMapper.readValue(objectMapper.writeValueAsString(payload), typeRef).asScala.toMap, auditLogEntry.target)
+
+    // odotetaan että parserointi valmis
+    Assertions.assertEquals(1, response.jobids.size())
+    waitUntilReady(response.jobids.asScala.head)
+
+    // uudelleenparserointi poistanut opiskeluoikeuden
+    Assertions.assertEquals(Map.empty, kantaOperaatiot.haeSuoritukset(ApiConstants.ESIMERKKI_OPPIJANUMERO))
+  }
+
+  @WithMockUser(value = "kayttaja", authorities = Array(SecurityConstants.SECURITY_ROOLI_REKISTERINPITAJA_FULL))
+  @Test def testUudelleenParseroiYTR(): Unit = {
+    // tallennetaan versio ja parseroitua dataa
+    val versio = kantaOperaatiot.tallennaJarjestelmaVersio(ApiConstants.ESIMERKKI_OPPIJANUMERO, SuoritusJoukko.YTR, Seq.empty, Instant.now()).get
+    val opiskeluoikeudet: Set[Opiskeluoikeus] = Set(YOOpiskeluoikeus(UUID.randomUUID(), YOTutkinto(UUID.randomUUID(), Koodi("arvo", "koodisto", None), VALMIS, None, Set.empty)))
+    kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, opiskeluoikeudet, Seq.empty)
+
+    // suoritetaan kutsu ja parseroidaan vastaus
+    val payload = new ReparsePayload().copy(ytr = true)
+    val result = mvc.perform(jsonPost(ApiConstants.DATASYNC_UUDELLEENPARSEROI_PATH, payload))
+      .andExpect(status().isOk).andReturn()
+    val response = objectMapper.readValue(result.getResponse.getContentAsString(Charset.forName("UTF-8")), classOf[ReparseSuccessResponse])
+
+    // katsotaan että kutsun tiedot tallentuvat auditlokiin
+    val auditLogEntry = getLatestAuditLogEntry()
+    Assertions.assertEquals(AuditOperation.Uudelleenparseroi.name, auditLogEntry.operation)
+    val typeRef = new TypeReference[java.util.Map[String, String]] {}
+    Assertions.assertEquals(objectMapper.readValue(objectMapper.writeValueAsString(payload), typeRef).asScala.toMap, auditLogEntry.target)
+
+    // odotetaan että parserointi valmis
+    Assertions.assertEquals(1, response.jobids.size())
+    waitUntilReady(response.jobids.asScala.head)
+
+    // uudelleenparserointi poistanut opiskeluoikeuden
+    Assertions.assertEquals(Map.empty, kantaOperaatiot.haeSuoritukset(ApiConstants.ESIMERKKI_OPPIJANUMERO))
+  }
+
+  @WithMockUser(value = "kayttaja", authorities = Array(SecurityConstants.SECURITY_ROOLI_REKISTERINPITAJA_FULL))
+  @Test def testUudelleenParseroiPerusopetuksenOppimaarat(): Unit = {
+    // tallennetaan versio ja parseroitua dataa
+    val versio = kantaOperaatiot.tallennaJarjestelmaVersio(ApiConstants.ESIMERKKI_OPPIJANUMERO, SuoritusJoukko.SYOTETTY_PERUSOPETUS, Seq.empty, Instant.now()).get
+    val opiskeluoikeudet: Set[Opiskeluoikeus] = Set(PerusopetuksenOpiskeluoikeus(
+      UUID.randomUUID(),
+      None,
+      ApiConstants.ESIMERKKI_OPPILAITOS_OID,
+      Set.empty,
+      None,
+      VALMIS,
+      List.empty
+    ))
+    kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, opiskeluoikeudet, Seq.empty)
+
+    // suoritetaan kutsu ja parseroidaan vastaus
+    val payload = new ReparsePayload().copy(perusopetuksenOppimaarat = true)
+    val result = mvc.perform(jsonPost(ApiConstants.DATASYNC_UUDELLEENPARSEROI_PATH, payload))
+      .andExpect(status().isOk).andReturn()
+    val response = objectMapper.readValue(result.getResponse.getContentAsString(Charset.forName("UTF-8")), classOf[ReparseSuccessResponse])
+
+    // katsotaan että kutsun tiedot tallentuvat auditlokiin
+    val auditLogEntry = getLatestAuditLogEntry()
+    Assertions.assertEquals(AuditOperation.Uudelleenparseroi.name, auditLogEntry.operation)
+    val typeRef = new TypeReference[java.util.Map[String, String]] {}
+    Assertions.assertEquals(objectMapper.readValue(objectMapper.writeValueAsString(payload), typeRef).asScala.toMap, auditLogEntry.target)
+
+    // odotetaan että parserointi valmis
+    Assertions.assertEquals(1, response.jobids.size())
+    waitUntilReady(response.jobids.asScala.head)
+
+    // uudelleenparserointi poistanut opiskeluoikeuden
+    Assertions.assertEquals(Map.empty, kantaOperaatiot.haeSuoritukset(ApiConstants.ESIMERKKI_OPPIJANUMERO))
+  }
+
+  @WithMockUser(value = "kayttaja", authorities = Array(SecurityConstants.SECURITY_ROOLI_REKISTERINPITAJA_FULL))
+  @Test def testUudelleenParseroiPerusopetuksenOppiaineenOppimaarat(): Unit = {
+    // tallennetaan versio ja parseroitua dataa
+    val versio = kantaOperaatiot.tallennaJarjestelmaVersio(ApiConstants.ESIMERKKI_OPPIJANUMERO, SuoritusJoukko.SYOTETYT_OPPIAINEET, Seq.empty, Instant.now()).get
+    val opiskeluoikeudet: Set[Opiskeluoikeus] = Set(PerusopetuksenOpiskeluoikeus(
+      UUID.randomUUID(),
+      None,
+      ApiConstants.ESIMERKKI_OPPILAITOS_OID,
+      Set.empty,
+      None,
+      VALMIS,
+      List.empty
+    ))
+    kantaOperaatiot.tallennaVersioonLiittyvatEntiteetit(versio, opiskeluoikeudet, Seq.empty)
+
+    // suoritetaan kutsu ja parseroidaan vastaus
+    val payload = new ReparsePayload().copy(perusopetuksenOppiaineet = true)
+    val result = mvc.perform(jsonPost(ApiConstants.DATASYNC_UUDELLEENPARSEROI_PATH, payload))
+      .andExpect(status().isOk).andReturn()
+    val response = objectMapper.readValue(result.getResponse.getContentAsString(Charset.forName("UTF-8")), classOf[ReparseSuccessResponse])
+
+    // katsotaan että kutsun tiedot tallentuvat auditlokiin
+    val auditLogEntry = getLatestAuditLogEntry()
+    Assertions.assertEquals(AuditOperation.Uudelleenparseroi.name, auditLogEntry.operation)
+    val typeRef = new TypeReference[java.util.Map[String, String]] {}
+    Assertions.assertEquals(objectMapper.readValue(objectMapper.writeValueAsString(payload), typeRef).asScala.toMap, auditLogEntry.target)
+
+    // odotetaan että parserointi valmis
+    Assertions.assertEquals(1, response.jobids.size())
+    waitUntilReady(response.jobids.asScala.head)
+
+    // uudelleenparserointi poistanut opiskeluoikeuden
+    Assertions.assertEquals(Map.empty, kantaOperaatiot.haeSuoritukset(ApiConstants.ESIMERKKI_OPPIJANUMERO))
+  }
+}
