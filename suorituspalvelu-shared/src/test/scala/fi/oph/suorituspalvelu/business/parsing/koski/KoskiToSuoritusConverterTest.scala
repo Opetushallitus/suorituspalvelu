ed025369a62e31e190cfeb75d5312329fdbaf550
Santeri Korri <santeri.korri@knowit.fi>
2026-02-03 16:34:29 +0200

    OPHSUPA-208 Korjattu mergeongelmat

--- suorituspalvelu-shared/src/test/scala/fi/oph/suorituspalvelu/business/parsing/koski/KoskiToSuoritusConverterTest.scala
+++ suorituspalvelu-shared/src/test/scala/fi/oph/suorituspalvelu/business/parsing/koski/KoskiToSuoritusConverterTest.scala
@@ -175,7 +175,7 @@ class KoskiToSuoritusConverterTest {
     val opiskeluoikeusEronnut = KoskiOpiskeluoikeus(
       "1.2.3",
       None,
-      KoskiOpiskeluoikeusTyyppi("arvo", "koodisto", None),
+      Some(KoskiOpiskeluoikeusTyyppi("arvo", "koodisto", None)),
       Some(KoskiOpiskeluoikeusTila(List(
         KoskiOpiskeluoikeusJakso(
           LocalDate.parse("2024-01-01"),
@@ -185,7 +185,8 @@ class KoskiToSuoritusConverterTest {
           LocalDate.parse("2024-06-15"),
           KoskiKoodi("eronnut", "koodisto", None, Kielistetty(None, None, None), None)
         )))),
-      Set.empty,
+      Some(Set.empty),
+      None,
       None
     )
     Assertions.assertEquals(Some(LocalDate.parse("2024-06-15")), KoskiToSuoritusConverter.parseKeskeytyminen(opiskeluoikeusEronnut))
@@ -194,7 +195,7 @@ class KoskiToSuoritusConverterTest {
     val opiskeluoikeusValmis = KoskiOpiskeluoikeus(
       "1.2.3",
       None,
-      KoskiOpiskeluoikeusTyyppi("arvo", "koodisto", None),
+      Some(KoskiOpiskeluoikeusTyyppi("arvo", "koodisto", None)),
       Some(KoskiOpiskeluoikeusTila(List(
         KoskiOpiskeluoikeusJakso(
           LocalDate.parse("2024-01-01"),
@@ -204,7 +205,8 @@ class KoskiToSuoritusConverterTest {
           LocalDate.parse("2024-06-15"),
           KoskiKoodi("valmistunut", "koodisto", None, Kielistetty(None, None, None), None)
         )))),
-      Set.empty,
+      Some(Set.empty),
+      None,
       None
     )
     Assertions.assertEquals(None, KoskiToSuoritusConverter.parseKeskeytyminen(opiskeluoikeusValmis))
@@ -213,9 +215,10 @@ class KoskiToSuoritusConverterTest {
     val opiskeluoikeusNoTila = KoskiOpiskeluoikeus(
       "1.2.3",
       None,
-      KoskiOpiskeluoikeusTyyppi("arvo", "koodisto", None),
+      Some(KoskiOpiskeluoikeusTyyppi("arvo", "koodisto", None)),
       None,
-      Set.empty,
+      Some(Set.empty),
+      None,
       None
     )
     Assertions.assertEquals(None, KoskiToSuoritusConverter.parseKeskeytyminen(opiskeluoikeusNoTila))
