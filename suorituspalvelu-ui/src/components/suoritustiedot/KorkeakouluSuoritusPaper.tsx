f73902128cd176a9fc25d8a3c2f49e5d759a4e81
Petteri Tolonen <petteri.tolonen@knowit.fi>
2026-02-09 09:24:40 +0200

    OPHSUPA-146: Näytetään korkeakoulu-suoritusten osasuoritukset listana käyttöliittymässä testausta varten

--- suorituspalvelu-ui/src/components/suoritustiedot/KorkeakouluSuoritusPaper.tsx
+++ suorituspalvelu-ui/src/components/suoritustiedot/KorkeakouluSuoritusPaper.tsx
@@ -1,17 +1,36 @@
-import { ophColors } from '@opetushallitus/oph-design-system';
+import { ophColors, OphTypography } from '@opetushallitus/oph-design-system';
 import { SuoritusInfoPaper } from './SuoritusInfoPaper';
 import { SuorituksenPerustiedotIndicator } from './SuorituksenPerustiedotIndicator';
 import type { KorkeakouluSuoritus } from '@/types/ui-types';
+import { useTranslations } from '@/hooks/useTranslations';
+import { isEmptyish } from 'remeda';
 
 export const KorkeakouluSuoritusPaper = ({
   suoritus,
 }: {
   suoritus?: KorkeakouluSuoritus | undefined;
 }) => {
+  const { translateKielistetty } = useTranslations();
   return (
     suoritus && (
       <SuoritusInfoPaper suoritus={suoritus} topColor={ophColors.red1}>
         <SuorituksenPerustiedotIndicator perustiedot={suoritus} />
+        {!isEmptyish(suoritus?.opintojaksot) && (
+          <>
+            <OphTypography variant="h5" sx={{ marginTop: 2 }}>
+              Opintosuoritukset
+            </OphTypography>
+            <ul>
+              {suoritus.opintojaksot?.map((opintojakso) => {
+                return (
+                  <li key={opintojakso.tunniste}>
+                    {translateKielistetty(opintojakso.nimi)}
+                  </li>
+                );
+              })}
+            </ul>
+          </>
+        )}
       </SuoritusInfoPaper>
     )
   );
