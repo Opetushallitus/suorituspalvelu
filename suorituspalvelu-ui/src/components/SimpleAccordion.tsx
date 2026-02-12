4c47f337f066181711cf33c880bd123c703db81d
Petteri Tolonen <petteri.tolonen@knowit.fi>
2026-02-12 16:48:06 +0200

    OPHSUPA-145: Lisätty haitari korkeakoulusuoritusten opintosuoritusten näyttämiseen

---
+++ suorituspalvelu-ui/src/components/SimpleAccordion.tsx
@@ -0,0 +1,62 @@
+import { ExpandMore } from '@mui/icons-material';
+import { Box } from '@mui/material';
+import { OphButton, ophColors } from '@opetushallitus/oph-design-system';
+import { useId, useState } from 'react';
+
+const TRANSITION_DURATION_MS = 200;
+
+export const SimpleAccordion = ({
+  titleOpen,
+  titleClosed,
+  children,
+}: {
+  titleOpen: React.ReactNode;
+  titleClosed: React.ReactNode;
+  children: React.ReactNode;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const accordionId = useId();
+  const contentId = `SimpleAccordionContent_${accordionId}`;
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        '&.MuiButton-icon': {
+          marginRight: 0.5,
+        },
+      }}
+    >
+      <OphButton
+        variant="text"
+        sx={{ fontWeight: 'normal', paddingX: 0 }}
+        startIcon={
+          <ExpandMore
+            sx={{
+              transform: isOpen ? 'rotate(180deg)' : 'none',
+              transition: `transform ${TRANSITION_DURATION_MS}ms ease`,
+              color: ophColors.black,
+            }}
+          />
+        }
+        onClick={() => setIsOpen((open) => !open)}
+        aria-controls={contentId}
+        aria-expanded={isOpen ? 'true' : 'false'}
+      >
+        {isOpen ? titleOpen : titleClosed}
+      </OphButton>
+      <Box
+        id={contentId}
+        sx={{
+          display: 'grid',
+          gridTemplateRows: isOpen ? '1fr' : '0fr',
+        }}
+      >
+        <Box sx={{ overflow: 'hidden' }}>{children}</Box>
+      </Box>
+    </Box>
+  );
+};
--- suorituspalvelu-ui/src/components/suoritustiedot/KorkeakouluSuoritusPaper.tsx
+++ suorituspalvelu-ui/src/components/suoritustiedot/KorkeakouluSuoritusPaper.tsx
@@ -1,27 +1,26 @@
-import { ophColors, OphTypography } from '@opetushallitus/oph-design-system';
+import { ophColors } from '@opetushallitus/oph-design-system';
 import { SuoritusInfoPaper } from './SuoritusInfoPaper';
 import { SuorituksenPerustiedotIndicator } from './SuorituksenPerustiedotIndicator';
 import type { KorkeakouluSuoritus } from '@/types/ui-types';
 import { useTranslations } from '@/hooks/useTranslations';
 import { isEmptyish } from 'remeda';
-
-const SHOW_OPINTOJAKSOT = false;
+import { SimpleAccordion } from '../SimpleAccordion';
 
 export const KorkeakouluSuoritusPaper = ({
   suoritus,
 }: {
   suoritus?: KorkeakouluSuoritus | undefined;
 }) => {
-  const { translateKielistetty } = useTranslations();
+  const { t, translateKielistetty } = useTranslations();
   return (
     suoritus && (
       <SuoritusInfoPaper suoritus={suoritus} topColor={ophColors.red1}>
         <SuorituksenPerustiedotIndicator perustiedot={suoritus} />
-        {SHOW_OPINTOJAKSOT && !isEmptyish(suoritus?.opintojaksot) && (
-          <>
-            <OphTypography variant="h5" sx={{ marginTop: 2 }}>
-              Opintosuoritukset
-            </OphTypography>
+        {!isEmptyish(suoritus?.opintojaksot) && (
+          <SimpleAccordion
+            titleClosed={t('oppija.nayta-opintosuoritukset')}
+            titleOpen={t('oppija.piilota-opintosuoritukset')}
+          >
             <ul>
               {suoritus.opintojaksot?.map((opintojakso) => {
                 return (
@@ -31,7 +30,7 @@ export const KorkeakouluSuoritusPaper = ({
                 );
               })}
             </ul>
-          </>
+          </SimpleAccordion>
         )}
       </SuoritusInfoPaper>
     )
