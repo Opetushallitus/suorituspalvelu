544b390bc5b728ec23ae6d6da53af0dae7827edd
August Kilponen <august.kilponen@gmail.com>
2026-03-04 20:49:18 +0200

    Lisätty virhesähköpostin lähetys yöllisille tausta-ajoille

--- suorituspalvelu-service/src/main/scala/fi/oph/suorituspalvelu/service/ErrorService.scala
+++ suorituspalvelu-service/src/main/scala/fi/oph/suorituspalvelu/service/ErrorService.scala
@@ -1,5 +1,7 @@
 package fi.oph.suorituspalvelu.service
 
+import fi.oph.suorituspalvelu.email.EmailService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 trait ErrorService {
@@ -7,7 +9,31 @@ trait ErrorService {
 }
 
 @Component
-class SupaErrorService extends ErrorService {
+class SupaErrorService(emailService: EmailService) extends ErrorService {
 
-  def reportErrors(jobName: String, errors: Seq[(String, Option[Exception])]): Unit = {}
+  private val LOG = LoggerFactory.getLogger(classOf[SupaErrorService])
+
+  private val EMAIL_ENABLED_JOBS = Set("ytr-refresh-aktiiviset", "virta-refresh-aktiiviset")
+
+  def reportErrors(jobName: String, errors: Seq[(String, Option[Exception])]): Unit = {
+    if errors.isEmpty then return
+
+    LOG.warn(s"Tausta-ajo $jobName päättyi ${errors.size} virheeseen")
+
+    if !EMAIL_ENABLED_JOBS.contains(jobName) then return
+
+    val htmlContent = s"<p>Tausta-ajo <strong>$jobName</strong> päättyi ${errors.size} virheeseen.</p>"
+    val exceptionMessages: Seq[String] = errors.map(_._1)
+
+    try {
+      emailService.sendErrorSummaryEmail(
+        subject = s"Suorituspalvelu: $jobName - ${errors.size} virhettä",
+        htmlContent = htmlContent,
+        exceptionMessages = exceptionMessages
+      )
+    } catch {
+      case e: Exception =>
+        LOG.error(s"Virheraportin sähköpostin lähetys epäonnistui jobille $jobName", e)
+    }
+  }
 }
