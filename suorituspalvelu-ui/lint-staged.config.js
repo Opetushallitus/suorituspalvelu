d37ae523b76024951b7f37ebe140c31a311f8743
August Kilponen <august.kilponen@gmail.com>
2026-03-04 21:49:34 +0200

    Revertoitu IDEA:n muutoksia

--- suorituspalvelu-ui/lint-staged.config.js
+++ suorituspalvelu-ui/lint-staged.config.js
@@ -7,10 +7,6 @@ const eslintCommand = 'eslint --fix --no-warn-ignored --max-warnings=0';
 const prettierCommand = 'prettier --write -u';
 
 export default {
-  '**/*.{js,mjs,cjs,jsx,ts,tsx}': (files) => {
-    const filtered = files.filter((f) => !f.endsWith('/types/backend.ts'));
-    if (filtered.length === 0) return [];
-    return [`${eslintCommand} ${filtered.join(' ')}`, `${prettierCommand} ${filtered.join(' ')}`];
-  },
+  '**/*.{js,mjs,cjs,jsx,ts,tsx}': [eslintCommand, prettierCommand],
   '!**/*.{js,mjs,cjs,jsx,ts,tsx}': prettierCommand,
 };
