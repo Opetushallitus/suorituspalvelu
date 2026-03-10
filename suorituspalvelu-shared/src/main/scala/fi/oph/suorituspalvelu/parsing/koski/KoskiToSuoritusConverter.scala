39c7ec55cca92b7106ba85f65ce52460649eabe5
Mikko Siukola <mikko.siukola@cybercom.com>
2026-03-10 12:49:04 +0200

    OPHSUPA-300 Fix aikuisten perusopetus oppiaineen oppimaara parsing

--- suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/parsing/koski/KoskiToSuoritusConverter.scala
+++ suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/parsing/koski/KoskiToSuoritusConverter.scala
@@ -605,7 +605,7 @@ object KoskiToSuoritusConverter {
       lahtokoulu = Lahtokoulu(aloitus.get, vahvistus.orElse(parseKeskeytyminen(opiskeluoikeus)), oppilaitos.oid, aloitus.map(_.getYear + 1), PERUSOPETUKSEEN_VALMISTAVA_OPETUS.defaultLuokka.get, supaTila, None, PERUSOPETUKSEEN_VALMISTAVA_OPETUS)
     )
   }
-  
+
   def toEbOppiaineenOsasuoritus(osaSuoritus: KoskiOsaSuoritus): EBOppiaineenOsasuoritus = {
     //Voiko eb-alaosasuorituksella olla useita arviointeja? Jos voi, voiko arvioinneilla olla erilaisia koodistoja? Käytetäänkö aina koodistoa arviointiasteikkoeuropeanschoolofhelsinkifinalmark?
     val parasArviointi: Option[KoskiArviointi] = {
@@ -809,7 +809,7 @@ object KoskiToSuoritusConverter {
   val SUORITUSTYYPPI_PERUSOPETUKSENOPPIMAARA                    = "perusopetuksenoppimaara"
   val SUORITUSTYYPPI_PERUSOPETUKSENVUOSILUOKKA                  = "perusopetuksenvuosiluokka"
   val SUORITUSTYYPPI_NUORTENPERUSOPETUKSENOPPIAINEENOPPIMAARA   = "nuortenperusopetuksenoppiaineenoppimaara"
-  val SUORITUSTYYPPI_AIKUISTENPERUSOPETUKSENOPPIAINEENOPPIMAARA = "aikuistenperusopetuksenoppiaineenoppimaara"
+  val SUORITUSTYYPPI_AIKUISTENPERUSOPETUKSENOPPIAINEENOPPIMAARA = "perusopetuksenoppiaineenoppimaara"
   val SUORITUSTYYPPI_PERUSOPETUKSEENVALMISTAVAOPETUS            = "perusopetukseenvalmistavaopetus"
   val SUORITUSTYYPPI_TELMA                                      = "telma"
   val SUORITUSTYYPPI_TUVAKOULUTUKSENSUORITUS                    = "tuvakoulutuksensuoritus"
