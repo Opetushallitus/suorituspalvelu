1b3880a6dee2e551b7cbaafc6ee8ec50c0bfe152
Santeri Korri <santeri.korri@knowit.fi>
2026-03-24 07:57:19 +0200

    OPHSUPA-140 Lisätty ensikertalaisuuden päättely

--- suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/integration/client/VTSClient.scala
+++ suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/integration/client/VTSClient.scala
@@ -6,6 +6,8 @@ import fi.vm.sade.javautils.nio.cas.CasClient
 import org.asynchttpclient.RequestBuilder
 import org.slf4j.LoggerFactory
 
+import com.fasterxml.jackson.core.`type`.TypeReference
+
 import scala.concurrent.Future
 import scala.jdk.javaapi.FutureConverters.asScala
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -15,6 +17,8 @@ case class OpintopolkuVastaanotto(personOid: String, hakuOid: String, hakukohdeO
 case class VanhaVastaanotto(personOid: String, hakukohde: String, vastaanottoaika: String)
 
 case class Vastaanotot(opintopolku: Seq[OpintopolkuVastaanotto], vanhat: Seq[VanhaVastaanotto])
+
+case class Ensikertalaisuus(personOid: String, paattyi: Option[String])
 
 class VTSClient(casClient: CasClient, environmentBaseUrl: String) {
 
@@ -29,6 +33,42 @@ class VTSClient(casClient: CasClient, environmentBaseUrl: String) {
     doGet(url).map(resultOpt => resultOpt.map(result => {
       mapper.readValue(result, classOf[Vastaanotot])
     }))
+  }
+
+  def fetchEnsikertalaisuudet(henkiloOids: Seq[String]): Future[Seq[Ensikertalaisuus]] = {
+    val url = environmentBaseUrl + "/valinta-tulos-service/ensikertalaisuus?koulutuksenAlkamiskausi=2014S"
+    doPost(url, henkiloOids).map(result => {
+      val typeRef = new TypeReference[Seq[Ensikertalaisuus]] {}
+      mapper.readValue(result, typeRef)
+    })
+  }
+
+  private def doPost(url: String, body: Object): Future[String] = {
+    LOG.debug(s"haetaan POST, $url")
+    val req = new RequestBuilder()
+      .setMethod("POST")
+      .setHeader("Content-Type", "application/json")
+      .setBody(mapper.writeValueAsString(body))
+      .setUrl(url)
+      .build()
+    try {
+      asScala(casClient.execute(req)).map {
+        case r if r.getStatusCode == 200 =>
+          r.getResponseBody()
+        case r =>
+          val errorStr = s"Haku valinta-tulos-servicestä epäonnistui: ${r.getStatusCode} ${r.getStatusText} ${r.getResponseBody()}"
+          LOG.error(
+            errorStr
+          )
+          throw new RuntimeException(errorStr)
+      }
+    } catch {
+      case e: Throwable =>
+        LOG.error(
+          s"Haku valinta-tulos-servicestä epäonnistui", e
+        )
+        Future.failed(e)
+    }
   }
 
   private def doGet(url: String): Future[Option[String]] = {
