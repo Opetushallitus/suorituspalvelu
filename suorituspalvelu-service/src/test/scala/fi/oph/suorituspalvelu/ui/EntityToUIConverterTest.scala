5439d8cc5b76e0c495602e90348419cbb39bca15
Santeri Korri <santeri.korri@knowit.fi>
2026-04-23 13:20:14 +0300

    OPHSUPA-220 Lisätty äidinkielen lisätieto käyttöliittymään

--- suorituspalvelu-service/src/test/scala/fi/oph/suorituspalvelu/ui/EntityToUIConverterTest.scala
+++ suorituspalvelu-service/src/test/scala/fi/oph/suorituspalvelu/ui/EntityToUIConverterTest.scala
@@ -572,6 +572,16 @@ class EntityToUIConverterTest {
           yksilollistetty = Some(false),
           rajattu = Some(false)
         ),
+        PerusopetuksenOppiaine(
+          tunniste = UUID.randomUUID(),
+          nimi = Kielistetty(Some("Äidinkieli"), None, None),
+          koodi = Koodi("AI", "koskioppiaineetyleissivistava", Some(1)),
+          arvosana = Koodi("9", "arviointiasteikkoyleissivistava", Some(1)),
+          kieli = Some(Koodi("AI1", "oppiaineaidinkielijakirjallisuus", Some(1))),
+          pakollinen = true,
+          yksilollistetty = Some(false),
+          rajattu = Some(false)
+        ),
       ),
       lahtokoulut = List.empty,
       syotetty = false,
@@ -592,6 +602,13 @@ class EntityToUIConverterTest {
           Koodisto("kielivalikoima"),
           List(
             KoodiMetadata("FI", "saksa")
+          )
+        ),
+        "AI1" -> fi.oph.suorituspalvelu.integration.client.Koodi(
+          "AI1",
+          Koodisto("oppiaineaidinkielijakirjallisuus"),
+          List(
+            KoodiMetadata("FI", "Suomen kieli ja kirjallisuus")
           )
         ),
       )
@@ -626,18 +643,20 @@ class EntityToUIConverterTest {
           Optional.empty
         )
       )),
-      oppiaineet = oppimaara.aineet.map(aine => fi.oph.suorituspalvelu.resource.ui.PerusopetuksenOppiaineUI(
+      oppiaineet = List("AI", "A1", "HI").map(koodi => oppimaara.aineet.find(_.koodi.arvo == koodi).get).map(aine => fi.oph.suorituspalvelu.resource.ui.PerusopetuksenOppiaineUI(
         tunniste = aine.tunniste,
         koodi = aine.koodi.arvo,
         nimi = PerusopetuksenOppiaineNimi(
-          if(aine.koodi.arvo=="A1") Optional.of("A1-kieli, saksa") else aine.nimi.fi.toJava,
+          if(aine.koodi.arvo=="A1") Optional.of("A1-kieli, saksa")
+          else if(aine.koodi.arvo=="AI") Optional.of("Äidinkieli, Suomen kieli ja kirjallisuus")
+          else aine.nimi.fi.toJava,
           aine.nimi.sv.toJava,
           aine.nimi.en.toJava
         ),
         kieli = aine.kieli.map(_.arvo).toJava,
         arvosana = aine.arvosana.arvo,
         valinnainen = !aine.pakollinen
-      )).toList.asJava,
+      )).asJava,
       syotetty = oppimaara.syotetty
     )), EntityToUIConverter.getOppijanTiedot(None, None, None, "1.2.3", "2.3.4", None, Set(PerusopetuksenOpiskeluoikeus(UUID.randomUUID(), Some("1.2.3"), "", Set(oppimaara), None, fi.oph.suorituspalvelu.business.SuoritusTila.VALMIS, List.empty)), DUMMY_ORGANISAATIOPROVIDER, koodistoProvider).perusopetuksenOppimaarat)
   }
