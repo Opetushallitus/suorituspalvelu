0b18e0c9916aef96c5ad1d361ba2d636b5b4eb46
Risto Salama <risto.salama@gofore.com>
2026-04-27 14:58:38 +0300

    Hae päätettävät kkopiskeluoikeudet WIP

--- suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/yos/YosPredicate.scala
+++ suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/yos/YosPredicate.scala
@@ -1,6 +1,10 @@
 package fi.oph.suorituspalvelu.yos
 
+import fi.oph.suorituspalvelu.business.{KKOpiskeluoikeus, Opiskeluoikeus}
+
 object YosPredicate {
+
+  private val YOSSIN_PIIRIIN_KUULUVAT_VIRTAOPISKELUOIKEUDEN_TILAT = Seq("1", "2", "4")
 
   def kuuluukoHakutoiveYosinPiiriin(hakutoive: YosHakutoive): Boolean = {
     hakutoive match {
@@ -9,6 +13,19 @@ object YosPredicate {
       case _ =>
         false
     }
+  }
+
+  def kuuluukoOpiskeluoikeusYosinPiiriin(opiskeluoikeus: Opiskeluoikeus): Boolean = {
+    opiskeluoikeus match
+      case oikeus: KKOpiskeluoikeus =>
+        YOSSIN_PIIRIIN_KUULUVAT_VIRTAOPISKELUOIKEUDEN_TILAT.contains(oikeus.virtaTila.arvo)
+        && oikeus.isTutkintoonJohtava
+        // TODO tilaustutkinto
+        // TODO jatkotutkinto
+        // TODO yhteis- tai kaksoistutkinto
+        // TODO maanpuolustuskorkeakoulu, poliisiammattikorkeakoulu tai Högskolan på Åland
+      case _ =>
+        false
   }
 
 }
