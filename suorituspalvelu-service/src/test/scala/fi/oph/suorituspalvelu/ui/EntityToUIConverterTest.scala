ee9a0a8c88d00eaf9eb0c0bd0a82403fe9028e8a
Risto Salama <risto.salama@gofore.com>
2026-04-30 13:04:07 +0300

    Rajapinta päätettävien opiskeluoikeuksien hakuun

--- suorituspalvelu-service/src/test/scala/fi/oph/suorituspalvelu/ui/EntityToUIConverterTest.scala
+++ suorituspalvelu-service/src/test/scala/fi/oph/suorituspalvelu/ui/EntityToUIConverterTest.scala
@@ -718,7 +718,10 @@ class EntityToUIConverterTest {
       myontaja = ORGANISAATION_OID,
       isTutkintoonJohtava = true,
       kieli = Some("fi"),
-      suoritukset = Set.empty
+      suoritukset = Set.empty,
+      rahoitusLahde = None,
+      nimi = Some(Kielistetty(Some("Sosekoulutus"), None, None)),
+      luokittelu = Some("6")
     )
 
     val virtaEiTutkintoonJohtavaOpiskeluoikeus = virtaOpiskeluoikeus.copy(
@@ -881,7 +884,10 @@ class EntityToUIConverterTest {
       myontaja = virtaTutkinto.myontaja,
       isTutkintoonJohtava = false,
       kieli = Some("fi"),
-      suoritukset = Set(virtaTutkinto)
+      suoritukset = Set(virtaTutkinto),
+      rahoitusLahde = None,
+      nimi = Some(Kielistetty(Some("Sosekoulutus"), None, None)),
+      luokittelu = Some("6")
     )
 
     Assertions.assertEquals(java.util.List.of(KKSuoritusUI(
