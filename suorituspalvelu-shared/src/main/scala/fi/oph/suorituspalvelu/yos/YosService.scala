4d1ea24429f7f0903776bd6163498a7ef697903c
Risto Salama <risto.salama@gofore.com>
2026-06-10 08:47:44 +0300

    YosService organisaatioOid -> tarjoaja muutos

--- suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/yos/YosService.scala
+++ suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/yos/YosService.scala
@@ -105,7 +105,7 @@ class YosService @Autowired (tarjontaIntegration: TarjontaIntegration,
   }
 
   private def muodostaYosHakutoive(haku: KoutaHaku, hakutoive: KoutaHakukohde): YosHakutoive = {
-    val organisaatioJaVanhemmat = List(hakutoive.organisaatioOid) ++ organisaatioProvider.haeKaikkiOrganisaationParenttienOidit(hakutoive.organisaatioOid)
+    val organisaatioJaVanhemmat = List(hakutoive.tarjoaja) ++ organisaatioProvider.haeKaikkiOrganisaationParenttienOidit(hakutoive.tarjoaja)
     val koulutusAste = getKoulutusAsteHakutoiveelle(hakutoive)
     YosHakutoive(haku.isKorkeakouluHaku, hakutoive.johtaaTutkintoon.getOrElse(false), haku.isJatkotutkinto,
       haku.isErasmusMundusTaiKaksoistutkinto, organisaatioJaVanhemmat, koulutusAste)
