66eb5ff7f4402862d2a4c2e938f1bdaa58423aa4
Risto Salama <risto.salama@gofore.com>
2026-08-19 13:25:34 +0300

    DbUtil runBlocking

---
+++ suorituspalvelu-shared/src/main/scala/fi/oph/suorituspalvelu/util/DBUtil.scala
@@ -0,0 +1,29 @@
+package fi.oph.suorituspalvelu.util
+
+import slick.jdbc.JdbcBackend.JdbcDatabaseDef
+
+import scala.concurrent.Await
+import slick.jdbc.PostgresProfile.api.*
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.Duration
+
+object DBUtil {
+  extension (database: JdbcDatabaseDef) {
+    def runBlocking[R](operations: DBIO[R], timeout: Duration): R = {
+      if (timeout.isFinite) {
+        Await.result(
+          database.run(
+            operations.withStatementParameters(statementInit = st => st.setQueryTimeout(timeout.toSeconds.toInt))
+          ),
+          timeout + Duration(1, TimeUnit.SECONDS)
+        )
+      } else {
+        Await.result(
+          database.run(operations),
+          timeout
+        )
+      }
+    }
+  }
+}
