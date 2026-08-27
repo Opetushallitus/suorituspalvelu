627262caef8b2440f5e63972bc3957619f824ce8
Marja Kari <marja.kari@amban.fi>
2026-08-27 14:16:46 +0300

    yos aikarajauksen konffi palautettu

--- suorituspalvelu-shared/src/test/scala/fi/oph/suorituspalvelu/business/yos/YosServiceTest.scala
+++ suorituspalvelu-shared/src/test/scala/fi/oph/suorituspalvelu/business/yos/YosServiceTest.scala
@@ -9,7 +9,7 @@ import fi.oph.suorituspalvelu.util.{KoodistoProvider, OrganisaatioProvider}
 import fi.oph.suorituspalvelu.yos.{YosConstants, YosHakutoive, YosService}
 import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertNotNull, assertTrue}
 import org.junit.jupiter.api.TestInstance.Lifecycle
-import org.junit.jupiter.api.{Assertions, BeforeEach, Disabled, Test, TestInstance}
+import org.junit.jupiter.api.{Assertions, BeforeEach, Test, TestInstance}
 import org.mockito.Mockito
 import fi.oph.suorituspalvelu.parsing.koski.Kielistetty
 import fi.oph.suorituspalvelu.resource.api.YosVirhe.{VIRHE_HAKUTOIVEEN_PAATTELYSSA, VIRHE_PAATETTAVIEN_OPISKELUOIKEUKSIEN_HAUSSA}
@@ -143,7 +143,6 @@ class YosServiceTest {
   // Yksityiskohtainen leikkuripäivä/-vuosirajojen testaus tehdään YosPredicateTest:ssä. Tässä varmistetaan
   // vain että YosService kytkee KoutaHaku/KoutaHakukohde-tiedot oikein YosHakutoiveelle, ja että YOS ei
   // ole voimassa -tilanne ei palauta virhettä vaan hakutoiveen jolla kuuluukoYosPiiriin == false.
-  @Disabled("Tilapäisesti aikarajaus pois kovakoodatusti") // FIXME
   @Test
   def eiVirhettaVaikkaYosEiOleVoimassaHaunHakuajanPerusteella(): Unit = {
     Mockito.when(organisaatioMock.haeKaikkiOrganisaationParenttienOidit(ORGANISAATIO_OID)).thenReturn(List.empty)
@@ -155,7 +154,6 @@ class YosServiceTest {
     assertFalse(tulos.map(_.kuuluukoYosPiiriin).getOrElse(true))
   }
 
-  @Disabled("Tilapäisesti aikarajaus pois kovakoodatusti") // FIXME
   @Test
   def haunAlkamisaikanaKaytetaanAikaisintaHakuaikaa(): Unit = {
     Mockito.when(organisaatioMock.haeKaikkiOrganisaationParenttienOidit(ORGANISAATIO_OID)).thenReturn(List.empty)
@@ -168,7 +166,6 @@ class YosServiceTest {
     assertFalse(service.kuuluukoVastaanotettavaHakutoiveYossinpiiriin(HAKU_OID, HAKUKOHDE_OID).map(_.kuuluukoYosPiiriin).getOrElse(true))
   }
 
-  @Disabled("Tilapäisesti aikarajaus pois kovakoodatusti") // FIXME
   @Test
   def eiVirhettaVaikkaYosEiOleVoimassaHakuaikojenPuuttuessa(): Unit = {
     Mockito.when(organisaatioMock.haeKaikkiOrganisaationParenttienOidit(ORGANISAATIO_OID)).thenReturn(List.empty)
@@ -179,7 +176,6 @@ class YosServiceTest {
     assertFalse(tulos.map(_.kuuluukoYosPiiriin).getOrElse(true))
   }
 
-  @Disabled("Tilapäisesti aikarajaus pois kovakoodatusti") // FIXME
   @Test
   def eiVirhettaVaikkaYosEiOleVoimassaHakukohteenAlkamisvuodenPerusteella(): Unit = {
     Mockito.when(organisaatioMock.haeKaikkiOrganisaationParenttienOidit(ORGANISAATIO_OID)).thenReturn(List.empty)
@@ -191,7 +187,6 @@ class YosServiceTest {
     assertFalse(tulos.map(_.kuuluukoYosPiiriin).getOrElse(true))
   }
 
-  @Disabled("Tilapäisesti aikarajaus pois kovakoodatusti") // FIXME
   @Test
   def eiVirhettaVaikkaHakukohteellaEiOlePaateltyaAlkamiskautta(): Unit = {
     Mockito.when(organisaatioMock.haeKaikkiOrganisaationParenttienOidit(ORGANISAATIO_OID)).thenReturn(List.empty)
